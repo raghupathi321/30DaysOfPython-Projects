c10af4bcdf319f684c272b4cae026476ca58b245
raghupathi321 <raghupathi1933@gmail.com>
2025-05-10 22:21:43 +0530

    madlibgame

---
+++ madlib_game.py
@@ -0,0 +1,10 @@
+def madlib():
+    noun1=input("Enter a noun: ")
+    place=input("Enter a place: ")
+    adjective=input("Enter an adjective: ")
+    verb=input("Enter a verb:")
+    preposition=input("Enter a preposition: ")
+    noun2=input("Enter a noun: ")
+    print(f"Once upon a time in {place}, there was a {adjective} {name1} who loved to {verb} {preposition} the {noun}.") 
+if __name__=='__main__':
+    madlib()
